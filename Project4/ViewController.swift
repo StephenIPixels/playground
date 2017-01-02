fca536dbf2222af63ff180e2ca923bac0098d90e
searthy <searthy@imac2014.local>
2017-01-02 13:02:08 -0800

    First Commit

--- Project4/ViewController.swift
+++ Project4/ViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    var webView: WKWebView
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
